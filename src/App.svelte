dfe5ec42d0523748535df2722fe22fbe49630a3f
Victor Bolaños Solís <viktorb.132@gmail.com>
2021-01-25 23:20:43 -0600

    Keyboard effect

---
+++ src/App.svelte
@@ -0,0 +1,67 @@
+<script>
+	import Keyboard from './Keyboard.svelte'
+	import { message } from './store.js'
+	export let name;
+
+	function reset() {
+		message.set('');
+	}
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<label for="">
+		{$message}
+		<button on:click="{reset}">remove</button>
+	</label>
+</main>
+
+<Keyboard/>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+	label {
+		position: relative;
+		padding: 0.5em 2.5em 0.5em 2em;
+		border-radius: 2px;
+		width: 60vw;
+		margin: auto;
+		user-select: none;
+	}
+	button {
+		position: absolute;
+		top: 0;
+		right: 0.2em;
+		width: 2em;
+		height: 100%;
+		background: no-repeat 50% 50% url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath fill='%23676778' d='M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M17,7H14.5L13.5,6H10.5L9.5,7H7V9H17V7M9,18H15A1,1 0 0,0 16,17V10H8V17A1,1 0 0,0 9,18Z'%3E%3C/path%3E%3C/svg%3E");
+		background-size: 1.4em 1.4em;
+		border: none;
+		opacity: 0;
+		transition: opacity 0.2s;
+		text-indent: -9999px;
+		cursor: pointer;
+	}
+
+	label:hover button {
+		opacity: 1;
+	}
+</style>
---
+++ src/Keyboard.svelte
@@ -0,0 +1,47 @@
+<script>
+	import { message } from './store.js'
+	let key;
+	let keyCode;
+	let noValidInputs = [16, 8, 13, 16, 20, 37, 38, 39,]
+
+	function handleKeydown(event) {
+		key = event.key;
+		keyCode = event.keyCode;
+		if (keyCode !== 16 && keyCode !== 8 && keyCode !== 13 & keyCode !== 16 && keyCode !== 20 && keyCode !== 37 && keyCode !== 38 && keyCode !== 39 && keyCode !== 40 && keyCode !== 9 && keyCode !== 17 && keyCode !== 225 && keyCode !== 93 && keyCode !== 18 && keyCode !== 123) {
+			message.update(n => n += key)
+		}
+	}
+</script>
+
+<style>
+	div {
+		display: flex;
+		height: 70%;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+	}
+
+	kbd {
+		background-color: #eee;
+		border-radius: 4px;
+		font-size: 6em;
+		padding: 0.2em 0.5em;
+		border-top: 5px solid rgba(255,255,255,0.5);
+		border-left: 5px solid rgba(255,255,255,0.5);
+		border-right: 5px solid rgba(0,0,0,0.2);
+		border-bottom: 5px solid rgba(0,0,0,0.2);
+		color: #555;
+	}
+</style>
+
+<svelte:window on:keydown={handleKeydown}/>
+
+<div style="text-align: center">
+	{#if key}
+		<kbd>{key === ' ' ? 'Space' : key}</kbd>
+		<p>{keyCode}</p>
+	{:else}
+		<p>Focus this window and press any key</p>
+	{/if}
+</div>
